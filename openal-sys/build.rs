d5ae5820d7802d20a537e4fe7c30199aae0f2845
Marco Satti <marcosatti@gmail.com>
2019-04-25 15:04:42 +0800

    Add CircleCI

--- openal-sys/build.rs
+++ openal-sys/build.rs
@@ -5,18 +5,32 @@ fn main() {
     let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
     let target = env::var("TARGET").unwrap();
 
-    bindgen::Builder::default()
-        .header("../external/openal/wrapper.h")
-        .clang_arg("-IC:/Devel/openal-soft-1.19.1-bin/include")
-        .generate()
-        .unwrap()
-        .write_to_file(out_path.join("openal-sys.rs"))
-        .unwrap();
+    let builder = bindgen::Builder::default()
+        .header("../external/openal/wrapper.h");
 
-    if target.contains("unknown-linux-gnu") {
+    if target.contains("linux") {
+        builder
+            .generate()
+            .unwrap()
+            .write_to_file(out_path.join("openal-sys.rs"))
+            .unwrap();
+    } else if target.contains("windows") {
+        builder
+            .clang_arg("-IC:/Devel/openal-soft-1.19.1-bin/include")
+            .generate()
+            .unwrap()
+            .write_to_file(out_path.join("openal-sys.rs"))
+            .unwrap();
+    } else {
+        unimplemented!("Unsupported target");
+    }
+
+    if target.contains("linux") {
         println!("cargo:rustc-link-lib=openal");
     } else if target.contains("windows") {
         println!("cargo:rustc-link-search=C:/Devel/openal-soft-1.19.1-bin/libs/Win64");
         println!("cargo:rustc-link-lib=openal32");
+    } else {
+        unimplemented!("Unsupported target");
     }
 }
--- opengl-sys/build.rs
+++ opengl-sys/build.rs
@@ -5,21 +5,37 @@ fn main() {
     let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
     let target = env::var("TARGET").unwrap();
 
-    bindgen::Builder::default()
-        .header("../external/opengl/wrapper.h")
-        .clang_arg("-IC:/Devel/mesa/include")
-        .clang_arg("-IC:/Devel/mesa-18.2.6-devel/include/x64")
-        .rustfmt_bindings(false)
-        .derive_debug(false)
-        .generate()
-        .unwrap()
-        .write_to_file(out_path.join("opengl-sys.rs"))
-        .unwrap();
+    let builder = bindgen::Builder::default()
+        .header("../external/opengl/wrapper.h");
 
-    if target.contains("unknown-linux-gnu") {
+    if target.contains("linux") {
+        builder
+            .rustfmt_bindings(false)
+            .derive_debug(false)
+            .generate()
+            .unwrap()
+            .write_to_file(out_path.join("opengl-sys.rs"))
+            .unwrap();
+    } else if target.contains("windows") {
+        builder
+            .clang_arg("-IC:/Devel/mesa/include")
+            .clang_arg("-IC:/Devel/mesa-18.2.6-devel/include/x64")
+            .rustfmt_bindings(false)
+            .derive_debug(false)
+            .generate()
+            .unwrap()
+            .write_to_file(out_path.join("opengl-sys.rs"))
+            .unwrap();
+    } else {
+        unimplemented!("Unsupported target");
+    }
+
+    if target.contains("linux") {
         println!("cargo:rustc-link-lib=GL");
     } else if target.contains("windows") {
         println!("cargo:rustc-link-search=C:/Devel/mesa-18.2.6-devel/lib/x64/gallium/targets/libgl-gdi");
         println!("cargo:rustc-link-lib=opengl32");
+    } else {
+        unimplemented!("Unsupported target");
     }
 }
