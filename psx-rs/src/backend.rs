8dfd4dd65aefb68ac7d501eaddafa116a82f418e
Marco Satti <marcosatti@gmail.com>
2020-03-21 21:11:17 +0800

    Add mute option

--- psx-rs/src/backend.rs
+++ psx-rs/src/backend.rs
@@ -58,6 +58,9 @@ pub(crate) fn terminate_video_backend() {
 /// Audio
 
 #[cfg(openal)]
+const MUTED: bool = true;
+
+#[cfg(openal)]
 static mut OPENAL_DEVICE: *mut openal_sys::ALCdevice = std::ptr::null_mut();
 
 #[cfg(openal)]
@@ -65,36 +68,40 @@ static mut OPENAL_CONTEXT: *mut openal_sys::ALCcontext = std::ptr::null_mut();
 
 #[cfg(openal)]
 pub(crate) fn initialize_audio_backend<'a: 'b, 'b>() -> AudioBackend<'a, 'b> {
-    use openal_sys::*;
-    use libpsx_rs::backends::context::BackendContext;
-
-    unsafe {
-        OPENAL_DEVICE = alcOpenDevice(std::ptr::null());
-        assert!(!OPENAL_DEVICE.is_null());
-        OPENAL_CONTEXT = alcCreateContext(OPENAL_DEVICE, std::ptr::null());
-        assert!(!OPENAL_CONTEXT.is_null());
-        alcMakeContextCurrent(OPENAL_CONTEXT);
-    }
-
-    // TODO: need to consider multithreading? It's a bit unclear, but doesn't look like it - probably implementation dependant...
-    let openal_acquire_context = || { &() };
-    let openal_release_context = || { };
-
-    openal_acquire_context();
-    unsafe { alListener3f(AL_POSITION as ALenum, 0.0, 0.0, 0.0) };
-    unsafe { alListener3f(AL_VELOCITY as ALenum, 0.0, 0.0, 0.0) };
-    unsafe { alListenerfv(AL_ORIENTATION as ALenum, [0.0, 0.0, -1.0, 0.0, 1.0, 0.0].as_ptr()) };
-    let openal_vendor_string = unsafe { std::ffi::CStr::from_ptr(alGetString(AL_VENDOR as ALenum)).to_string_lossy().into_owned() };
-    let openal_version_string = unsafe { std::ffi::CStr::from_ptr(alGetString(AL_VERSION as ALenum)).to_string_lossy().into_owned() };
-    let openal_renderer_string = unsafe { std::ffi::CStr::from_ptr(alGetString(AL_RENDERER as ALenum)).to_string_lossy().into_owned() };
-    log::info!("Audio initialized: {}, {}, {}", openal_vendor_string, openal_version_string, openal_renderer_string);
-    openal_release_context();
-
-    AudioBackend::Openal(
-        openal::BackendParams {
-            context: BackendContext::new(Box::new(openal_acquire_context), Box::new(openal_release_context)),
+    if !MUTED {
+        use openal_sys::*;
+        use libpsx_rs::backends::context::BackendContext;
+    
+        unsafe {
+            OPENAL_DEVICE = alcOpenDevice(std::ptr::null());
+            assert!(!OPENAL_DEVICE.is_null());
+            OPENAL_CONTEXT = alcCreateContext(OPENAL_DEVICE, std::ptr::null());
+            assert!(!OPENAL_CONTEXT.is_null());
+            alcMakeContextCurrent(OPENAL_CONTEXT);
         }
-    )
+    
+        // TODO: need to consider multithreading? It's a bit unclear, but doesn't look like it - probably implementation dependant...
+        let openal_acquire_context = || { &() };
+        let openal_release_context = || { };
+    
+        openal_acquire_context();
+        unsafe { alListener3f(AL_POSITION as ALenum, 0.0, 0.0, 0.0) };
+        unsafe { alListener3f(AL_VELOCITY as ALenum, 0.0, 0.0, 0.0) };
+        unsafe { alListenerfv(AL_ORIENTATION as ALenum, [0.0, 0.0, -1.0, 0.0, 1.0, 0.0].as_ptr()) };
+        let openal_vendor_string = unsafe { std::ffi::CStr::from_ptr(alGetString(AL_VENDOR as ALenum)).to_string_lossy().into_owned() };
+        let openal_version_string = unsafe { std::ffi::CStr::from_ptr(alGetString(AL_VERSION as ALenum)).to_string_lossy().into_owned() };
+        let openal_renderer_string = unsafe { std::ffi::CStr::from_ptr(alGetString(AL_RENDERER as ALenum)).to_string_lossy().into_owned() };
+        log::info!("Audio initialized: {}, {}, {}", openal_vendor_string, openal_version_string, openal_renderer_string);
+        openal_release_context();
+    
+        AudioBackend::Openal(
+            openal::BackendParams {
+                context: BackendContext::new(Box::new(openal_acquire_context), Box::new(openal_release_context)),
+            }
+        )
+    } else {
+        AudioBackend::None
+    }
 }
 
 #[cfg(not(openal))]
@@ -104,13 +111,16 @@ pub(crate) fn initialize_audio_backend<'a: 'b, 'b>() -> AudioBackend<'a, 'b> {
 
 #[cfg(openal)]
 pub(crate) fn terminate_audio_backend() {
-    use openal_sys::*;
+    if !MUTED {
+        use openal_sys::*;
     
-    unsafe {
-        assert!(!OPENAL_CONTEXT.is_null());
-        alcDestroyContext(OPENAL_CONTEXT);
-        assert!(!OPENAL_DEVICE.is_null());
-        alcCloseDevice(OPENAL_DEVICE);
+        unsafe {
+            assert!(!OPENAL_CONTEXT.is_null());
+            alcDestroyContext(OPENAL_CONTEXT);
+            assert!(!OPENAL_DEVICE.is_null());
+            alcCloseDevice(OPENAL_DEVICE);
+        }
+    } else {
     }
 }
 
