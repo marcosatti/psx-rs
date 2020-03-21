8d033d8e53bcbd05761cf4114c1455f223108991
Marco Satti <marcosatti@gmail.com>
2020-03-21 11:46:27 +0800

    Fix openal teardown process

--- libpsx-rs/src/backends/audio/openal.rs
+++ libpsx-rs/src/backends/audio/openal.rs
@@ -34,15 +34,12 @@ pub fn teardown(backend_params: &BackendParams) {
     unsafe {
         if INITIALIZED {
             for i in 0..VOICES_COUNT {
+                alSourceStop(SOURCES[i]);
                 alSourcei(SOURCES[i], AL_BUFFER as ALenum, 0);
             }
 
             alDeleteBuffers(rendering::BUFFERS.len() as ALsizei, rendering::BUFFERS.as_mut_ptr());
             alDeleteSources(rendering::SOURCES.len() as ALsizei, rendering::SOURCES.as_mut_ptr());
-    
-            if alGetError() != AL_NO_ERROR as ALenum {
-                panic!("Error tearing down OpenAL audio backend");
-            }
             
             INITIALIZED = false;
         }
