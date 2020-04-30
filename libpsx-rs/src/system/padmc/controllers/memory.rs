9e8f382c0e916c7af87c93301b3aa172933b43f2
Marco Satti <marcosatti@gmail.com>
2020-04-30 21:23:43 +0800

    Work in progress

--- libpsx-rs/src/system/padmc/controllers/memory.rs
+++ libpsx-rs/src/system/padmc/controllers/memory.rs
@@ -17,15 +17,11 @@ pub fn padmc1040_write_u8(state: &State, offset: u32, value: u8) -> WriteResult 
 pub fn padmc1040_read_u32(_state: &State, offset: u32) -> ReadResult<u32> {
     assert_eq!(offset, 0);
     unimplemented!();
-    // warn!("PADMC RX FIFO u32 preview reads not properly implemented");
-    // rx_fifo.read_one().map(|v| v as u32).map_err(|_| ReadError::Empty)
 }
 
 pub fn padmc1040_write_u32(_state: &State, offset: u32, _value: u32) -> WriteResult {
     assert_eq!(offset, 0);
     unimplemented!();
-    // let value_u8 = value as u8;
-    // tx_fifo.write_one(value_u8).map_err(|_| WriteError::Full)
 }
 
 pub fn stat_read_u16(state: &State, offset: u32) -> ReadResult<u16> {
